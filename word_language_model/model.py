2f847a01bcf8dd44001090eca15679afeead0840
Riko Nyberg <riko.nyberg@aalto.fi>
2019-08-30 09:46:03 +0300

    Remove default that is never used

--- word_language_model/model.py
+++ word_language_model/model.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 class RNNModel(nn.Module):
     """Container module with an encoder, a recurrent module, and a decoder."""
 
-    def __init__(self, rnn_type, ntoken, ninp, nhid, nlayers, dropout=0.5, tie_weights=False):
+    def __init__(self, rnn_type, ntoken, ninp, nhid, nlayers, dropout, tie_weights=False):
         super(RNNModel, self).__init__()
         self.drop = nn.Dropout(dropout)
         self.encoder = nn.Embedding(ntoken, ninp)
