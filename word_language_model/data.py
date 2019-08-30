4a44e6c9b512f1a7ee94ca14e5644df364431c54
Riko Nyberg <riko.nyberg@aalto.fi>
2019-08-30 09:45:02 +0300

    Remove <s></s> tags

--- word_language_model/data.py
+++ word_language_model/data.py
@@ -30,7 +30,7 @@ class Corpus(object):
         # Add words to the dictionary
         with open(path, 'r', encoding="utf8") as f:
             for line in f:
-                words = line.split() + ['<eos>']
+                words = line.replace('<s>', '').replace('</s>', '').split() + ['<eos>']
                 for word in words:
                     self.dictionary.add_word(word)
 
@@ -38,7 +38,7 @@ class Corpus(object):
         with open(path, 'r', encoding="utf8") as f:
             idss = []
             for line in f:
-                words = line.split() + ['<eos>']
+                words = line.replace('<s>', '').replace('</s>', '').split() + ['<eos>']
                 ids = []
                 for word in words:
                     ids.append(self.dictionary.word2idx[word])
